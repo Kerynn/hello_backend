58bd412833f10c1f7a8c1cf96d123ad3a185cfe4
Kerynn Davis <kerynn.davis@gmail.com>
2023-07-10 09:56:32 -0700

    add rest framework

--- customers/views.py
+++ customers/views.py
@@ -1,18 +1,22 @@
 from customers.models import Customer
 from django.http.response import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
     return JsonResponse({'customers': serializer.data})
 
+@api_view(['GET', 'POST', 'DELETE'])
 def customer(request, id):
     try: 
         data = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
-        raise Http404('Customer does not exist')
+        return Response(status=status.HTTP_404_NOT_FOUND)
     serializer = CustomerSerializer(data)
-    return JsonResponse({'customer': serializer.data})
+    return Response({'customer': serializer.data})
 
     
